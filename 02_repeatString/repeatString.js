30481e60230c66298e312da0a7aac84c08ecf606
TimAde18 <aoadewole18@gmail.com>
2023-09-05 16:16:49 -0500

    New Feature: Completed Problems 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let i = 0;
+    let resultString = "";
+    if (num < 0) {
+        resultString = "ERROR";
+        } else {
+        while (i < num) {
+            resultString += string
+            i++;
+        }
+    }
+    return resultString;
 };
 
 // Do not edit below this line
